2c685c5ab83131c873757923fdfd5307289dc212
kendell <KTibow@users.noreply.github.com>
2020-05-19 14:28:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-
+# home
 @app.route('/')
 def hello():
     return open("game/welcome.html", "r").read()
+# robots.txt
+@app.route('/robots.txt')
+def robo():
+    return open("game/robots.txt", "r").read()
