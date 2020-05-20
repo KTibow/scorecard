16ad4c9dee501e022ac3caaaf23b603214c3fca6
kendell <KTibow@users.noreply.github.com>
2020-05-20 09:12:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 app = Flask(__name__)
 
 # home
@@ -9,3 +10,7 @@ def hello():
 @app.route('/robots.txt')
 def robo():
     return open("game/robots.txt", "r").read()
+# favicon.ico
+def ico():
+    return send_from_directory(os.path.join(app.root_path, 'game'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
