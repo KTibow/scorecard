b0e1e5535a9ca11cb38b387454ace57e1800af55
kendell <KTibow@users.noreply.github.com>
2020-05-21 07:33:14 -0700

    Update sw.js

--- game/browserfiles/sw.js
+++ game/browserfiles/sw.js
@@ -1,11 +1,13 @@
+var cacheName = "escore-v3";
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open("pwaescorecard-v2").then((cache) => {
+    caches.open(cacheName).then((cache) => {
       console.log('[Service Worker] Caching caches');
       return cache.addAll(['/', 'https://fonts.googleapis.com/css?family=Open+Sans',
                            '/android-icon-192x192.png', '/ms-icon-310x310.png',
-                           '/ms-icon-150x150.png', '/favicon-96x96.png']);
+                           '/ms-icon-150x150.png', '/favicon-96x96.png',
+                           '/welcome.css', '/install.js']);
     })
   );
 });
@@ -14,7 +16,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((r) => {
           console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then((response) => {
-                return caches.open("pwaescorecard-v2").then((cache) => {
+                return caches.open(cacheName).then((cache) => {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
