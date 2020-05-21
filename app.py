6c0d67c6f33ef252a4c958755b4cb3bab2163933
kendell <KTibow@users.noreply.github.com>
2020-05-20 18:44:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_from_directory
-import os
+import os, magic
 app = Flask(__name__)
 # ========== WEB INTERFACE ==========
 # home
@@ -18,3 +18,11 @@ def robo():
 def ico():
     return send_from_directory(os.path.join(app.root_path, 'game'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# ============ FAVICONS ============
+def make_sender(path):
+    def f():
+        return send_from_directory(os.path.join(app.root_path, 'game'),
+                                   path, mimetype='image/vnd.microsoft.icon')
+    return f
+for file in ['/android-icon-36x36.png', '/android-icon-48x48.png', '/android-icon-72x72.png', '/android-icon-96x96.png']:
+    app.add_url_rule(file, file, make_sender(file))
