1189b9dcb1ed6e98417b3cd5f7a216e37069f712
kendell <KTibow@users.noreply.github.com>
2020-05-21 16:56:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ for file in ['/robots.txt', '/android-icon-36x36.png',
              '/browserconfig.xml', '/favicon-16x16.png', '/sw.js',
              '/favicon-32x32.png', '/favicon-96x96.png',
              '/favicon.ico', '/manifest.json', '/maskable_icon.png',
+             '/r_maskable_icon.png', '/r_maskable_icon(1).png', '/r_maskable_icon(2).png',
              '/ms-icon-70x70.png', '/ms-icon-144x144.png',
              '/ms-icon-150x150.png', '/ms-icon-310x310.png']:
     app.add_url_rule(file, file, make_sender(file, "browserfiles"))
