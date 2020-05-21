44079dd4eeda94845135f43856483de12bff5db9
kendell <KTibow@users.noreply.github.com>
2020-05-20 19:07:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,25 @@ def hello():
 # ============== API ================
 
 # ========== BROWSER FILES ==========
-# robots.txt
-@app.route('/robots.txt')
-def robo():
-    return open("game/robots.txt", "r").read()
-# favicon.ico
-@app.route('/favicon.ico')
-def ico():
-    return send_from_directory(os.path.join(app.root_path, 'game'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-# ============ FAVICONS ============
 def make_sender(path):
     def f():
-        print("I got called to do "+os.path.join(app.root_path, 'game'+path))
         mimetype = magic.from_file(os.path.join(app.root_path, 'game'+path), mime=True)
-        return send_from_directory(os.path.join(app.root_path, 'game'), path.replace("/", ""), mimetype=mimetype)
+        print("I got called to do "+os.path.join(app.root_path, 'game'+path)+" with mimetype of "+mimetype)
+        return send_from_directory(os.path.join(app.root_path, 'game'),
+                                   path.replace("/", ""), mimetype=mimetype)
     return f
-for file in ['/android-icon-36x36.png', '/android-icon-48x48.png', '/android-icon-72x72.png', '/android-icon-96x96.png']:
+for file in ['robots.txt', 'favicon.ico', '/android-icon-36x36.png',
+            '/android-icon-48x48.png', '/android-icon-72x72.png',
+            '/android-icon-96x96.png', '/android-icon-144x144.png',
+            '/android-icon-192x192.png', '/apple-icon.png',
+            '/apple-icon-57x57.png', '/apple-icon-60x60.png',
+            '/apple-icon-72x72.png', '/apple-icon-72x72.png',
+            '/apple-icon-76x76.png', '/apple-icon-114x114.png',
+            '/apple-icon-120x120.png', '/apple-icon-144x144.png',
+            '/apple-icon-152x152.png', '/apple-icon-180x180.png',
+            '/apple-icon-precomposed.png', '/browserconfig.xml',
+            '/favicon-16x16.png', '/favicon-32x32.png',
+            '/favicon-96x96.png', '/ms-icon-70x70.png',
+            '/ms-icon-144x144.png', '/ms-icon-150x150.png',
+            '/ms-icon-310x310.png']:
     app.add_url_rule(file, file, make_sender(file))
