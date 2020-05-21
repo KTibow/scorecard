093c55054a658366857b657b7367a8db2c084bd3
kendell <KTibow@users.noreply.github.com>
2020-05-21 14:51:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 # ============== INIT ==============
 from flask import Flask, send_from_directory, request, redirect
-from flask_minify import minify
 import os, magic
 app = Flask(__name__)
-minify(app=app, html=True, js=True, cssless=True, caching_limit=0)
 def make_sender(path, dir):
     def f():
         mimetype = magic.from_file(os.path.join(app.root_path, 'game/'+dir+path), mime=True)
