180e2c181f0f042560f9eb56a59044e750ddd51d
kendell <KTibow@users.noreply.github.com>
2020-05-20 18:51:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def ico():
 # ============ FAVICONS ============
 def make_sender(path):
     def f():
+        print("I got called to do "+path)
         return send_from_directory(os.path.join(app.root_path, 'game'),
-                                   path, mimetype='image/vnd.microsoft.icon')
+                                   path, mimetype=magic.from_file(path, mime=True))
     return f
 for file in ['/android-icon-36x36.png', '/android-icon-48x48.png', '/android-icon-72x72.png', '/android-icon-96x96.png']:
     app.add_url_rule(file, file, make_sender(file))
