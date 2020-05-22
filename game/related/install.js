a6dd6729aaa252faa7d5f5ff4c1e56f9006b8294
kendell <KTibow@users.noreply.github.com>
2020-05-22 08:51:57 -0700

    Update install.js

--- game/related/install.js
+++ game/related/install.js
@@ -1,38 +1,46 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(function(reg) {
-        function listenForWaitingServiceWorker(reg, callback) {
-          function awaitStateChange() {
+var confirming = false;
+var globreg;
+
+function checkVersion(reg) {
+    function listenForWaitingServiceWorker(reg, callback) {
+        function awaitStateChange() {
             reg.installing.addEventListener('statechange', function() {
-              if (this.state === 'installed') callback(reg);
+                if (this.state === 'installed') callback(reg);
             });
-          }
-          if (!reg) return;
-          if (reg.waiting) return callback(reg);
-          if (reg.installing) awaitStateChange();
-          reg.addEventListener('updatefound', awaitStateChange);
         }
-        // reload once when the new Service Worker starts activating
-        var refreshing;
-        navigator.serviceWorker.addEventListener('controllerchange',
-          function() {
+        if (!reg) return;
+        if (reg.waiting) return callback(reg);
+        if (reg.installing) awaitStateChange();
+        reg.addEventListener('updatefound', awaitStateChange);
+    }
+    var refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange',
+        function() {
             if (refreshing) return;
             refreshing = true;
             window.location.reload();
-          }
-        );
-        function promptUserToRefresh(reg) {
-          // this is just an example
-          // don't use window.confirm in real life; it's terrible
-          if (window.confirm("Refresh all tabs now and get the latest version of the ClueCard?")) {
-            reg.waiting.postMessage('skipWaiting');
-          }
         }
-        listenForWaitingServiceWorker(reg, promptUserToRefresh);
-    }).catch(err => console.log("service worker not registered", err));
-  });
+    );
+
+    function promptUserToRefresh(reg) {
+        if (!confirming) {
+            confirming = true;
+            if (window.confirm("Refresh all tabs now and get the latest version of the ClueCard?")) {
+                reg.waiting.postMessage('skipWaiting');
+            }
+            confirming = false;
+        }
+    }
+    listenForWaitingServiceWorker(reg, promptUserToRefresh);
+}
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then(function(reg) {
+                setTimeout(5000, checkVersion, reg);
+            }).catch (err = > console.log("service worker not registered", err));
+    });
 }
 window.addEventListener("load", function() {
     let deferredPrompt;
@@ -40,29 +48,29 @@ window.addEventListener("load", function() {
     const addAlt = document.getElementById("add2hsalt");
     addBtn.style.display = 'none';
     addAlt.style.display = 'none';
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      addBtn.style.display = 'block';
-      addAlt.style.display = 'block';
-      addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        addAlt.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the A2HS prompt');
-            } else {
-              console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-          });
-      });
-    });            
+    window.addEventListener('beforeinstallprompt', (e) = > {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        addBtn.style.display = 'block';
+        addAlt.style.display = 'block';
+        addBtn.addEventListener('click', (e) = > {
+            // hide our user interface that shows our A2HS button
+            addBtn.style.display = 'none';
+            addAlt.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) = > {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+        });
+    });
 });
