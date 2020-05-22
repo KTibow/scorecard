ad0bebe6d4a454dd7a8fc387bf2a390639c868ad
kendell <KTibow@users.noreply.github.com>
2020-05-21 18:58:16 -0700

    Update sw.js

--- game/browserfiles/sw.js
+++ game/browserfiles/sw.js
@@ -1,43 +1,41 @@
-var cacheName = "e-score-v8";
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installing...');
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('Service Worker Caching caches');
-      return cache.addAll(['/', '/welcome.css', '/install.js',
-                           '/sw.js', '/favicon.ico',
-                           '/manifest.json', '/maskable_icon.png',
-                           '/r_maskable_icon(1).png', '/r_maskable_icon(2).png',
-                           '/maskable_icon.png', '/r_maskable_icon.png',
-                           '/favicon-16x16.png', 'favicon-32x32.png']);
-    })
-  );
-});
-self.addEventListener('fetch', (e) = > {
-    e.respondWith(
-        caches.match(e.request).then((r) = > {
-            console.log('Service Worker Fetching resource ' + e.request.url);
-            return r || fetch(e.request).then((response) = > {
-                return caches.open(cacheName).then((cache) = > {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
+var cacheName = "e-score-v9";
+self.addEventListener('install', (e) = > {
+    console.log('Service Worker: Installing...');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) = > {
+            console.log('Service Worker: Caching caches...');
+            return cache.addAll(['/', '/welcome.css', '/install.js',
+                '/sw.js', '/favicon.ico',
+                '/manifest.json', '/maskable_icon.png',
+                '/r_maskable_icon(1).png', '/r_maskable_icon(2).png',
+                '/maskable_icon.png', '/r_maskable_icon.png',
+                '/favicon-16x16.png', 'favicon-32x32.png'
+            ]);
         })
     );
 });
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-        if(key !== cacheName) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open(cacheName).then(function(cache) {
+            console.log('Service Worker: Trying to cache ' + event.request.url + '...');
+            cache.add(event.request.url);
+        }).
+        catch (function() {}); fetch(event.request).
+        catch (function() {
+            console.log('Service Worker: Returning cache ' + event.request.url + '...');
+            return caches.match(event.request);
+        })
+    );
 });
-addEventListener('message', messageEvent => {
-  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+self.addEventListener('activate', (e) = > {
+    e.waitUntil(
+        caches.keys().then((keyList) = > {
+            return Promise.all(keyList.map((key) = > {
+                if (key !== cacheName) {
+                    console.log("Service Worker: Bye-Bye " + key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 });
