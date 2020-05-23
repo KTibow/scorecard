0f36bb514e66c9f2fcd297759239f10c993f6427
kendell <KTibow@users.noreply.github.com>
2020-05-23 10:57:45 -0700

    Update sw.js

--- game/browserfiles/sw.js
+++ game/browserfiles/sw.js
@@ -1,4 +1,4 @@
-var cacheName = "clue-card-v2.2";
+var cacheName = "clue-card-v2.3";
 console.log("Service Worker: Hello there!");
 self.addEventListener('install', (e) => {
     console.log('Service Worker: Installing...');
