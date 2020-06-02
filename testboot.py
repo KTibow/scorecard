309a482de0ca31e0a3501272cb47170f8d627a06
kendell <KTibow@users.noreply.github.com>
2020-06-02 12:54:16 -0700

    Shorten sleep time + messages

--- testboot.py
+++ testboot.py
@@ -1,8 +1,12 @@
+print("Loading...")
 exec(open("app.py", "r").read())
 from multiprocessing import Process
 from time import sleep
 server = Process(target=app.run)
 server.start()
-sleep(20)
+print("Waiting for 5 seconds...")
+sleep(5)
+print("Done!")
 server.terminate()
 server.join()
+print("Bye!")
