81934058bac8de32d9d891d58205a44cb556796c
kendell <KTibow@users.noreply.github.com>
2020-06-07 08:14:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def walk():
             if 'html' not in file:
                 pys.append([p.replace("game/", ""), "/"+file])
     return pys
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 @app.before_request
 def http_redir():
     if request.headers["X-Forwarded-Proto"] == "http":
