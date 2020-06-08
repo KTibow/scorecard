9cd3c37b6ae2d1a1ab21b1855fd6694f2b21900f
kendell <KTibow@users.noreply.github.com>
2020-06-08 11:59:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,62 @@
 # ============== INIT ==============
 from flask import Flask, send_from_directory, request, redirect, url_for
 from flask_minify import minify
+from user_agents import parse as ua_parse
 from github.MainClass import Github
-import os, magic, json, random
+import os, magic, json, random, requests
 app = Flask(__name__)
 minify(app=app, html=True, js=True, cssless=True, static=True, caching_limit=0)
 gg = Github(os.getenv("GITHUB_VERSION_PAT"))
 def make_sender(path, dir):
     def f():
-        mimetype = magic.from_file(os.path.join(app.root_path, 'game/'+dir+path), mime=True)
+        mimetype = magic.from_file(os.path.join(app.root_path, "game/"+dir+path), mime=True)
         if "js" in path:
             mimetype = "application/javascript"
         if "css" in path:
             mimetype = "text/css"
-        return send_from_directory(os.path.join(app.root_path, 'game/'+dir),
+        return send_from_directory(os.path.join(app.root_path, "game/"+dir),
                                    path.replace("/", ""), mimetype=mimetype)
     return f
 def walk():
     pys = []
-    for p, d, f in os.walk('game'):
+    for p, d, f in os.walk("game"):
         for file in f:
-            if 'html' not in file:
+            if "html" not in file:
                 pys.append([p.replace("game/", ""), "/"+file])
     return pys
 def has_no_empty_params(rule):
     defaults = rule.defaults if rule.defaults is not None else ()
     arguments = rule.arguments if rule.arguments is not None else ()
     return len(defaults) >= len(arguments)
+def track_view(page, ip, agent):
+    data = {
+        "v": "1",
+        "tid": "UA-165004437-2",
+        "cid": "555",
+        "t": "pageview",
+        "dh": "tank-scorecard.herokuapp.com",
+        "dp": page
+    }
+    if ip is not None:
+        data['uip'] = ip
+    if ua is not None:
+        data['ua'] = agent
+    response = requests.post(
+        'https://www.google-analytics.com/collect', data=data)
+    response.raise_for_status()
 @app.before_request
-def http_redir():
+def before_req():
     if request.headers["X-Forwarded-Proto"] == "http":
         return redirect(request.url.replace("http", "https"), code=301)
+    ua = None
+    ua_add = ""
+    if "User-Agent" in request.headers:
+        ua = request.headers["User-Agent"]
+        ua_add = ", "+str(ua_parse(ua))
+    ip = request.remote_addr
+    print("Hit from "+ip+ua_add)
+    track_view(request.url
+    
 # ========== WEB INTERFACE ==========
 # home
 @app.route('/')
