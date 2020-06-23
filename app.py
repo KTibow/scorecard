10f261aee02988e94ffed15957250ccf7c35320f
kendell <KTibow@users.noreply.github.com>
2020-06-22 17:36:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def before_req():
 def after_req(response):
     response.headers["Content-Security-Policy"] = "default-src https: 'unsafe-eval' 'unsafe-inline'; object-src 'none'"
     response.headers["Content-Security-Policy-Report-Only"] = "default-src https:"
-    if response.status_code == 200:
+    if response.status_code != 301:
         response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains; preload"
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
