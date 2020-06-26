016752e64934ade90d59b14eb992fac0e4d6780b
kendell <KTibow@users.noreply.github.com>
2020-06-26 07:22:23 -0700

    Update testboot.py

--- testboot.py
+++ testboot.py
@@ -3,6 +3,7 @@ exec(open("app.py", "r").read())
 from multiprocessing import Process
 from time import sleep, time
 from urllib.request import urlopen
+debuggy = True
 server = Process(target=app.run)
 server.start()
 stime = time()
@@ -10,8 +11,9 @@ print("Waiting for 2 seconds...")
 sleep(2)
 print("Pinging the server...")
 rpy = urlopen("http://127.0.0.1:5000/")
-print("Waiting the other part of 3.5 seconds...")
-sleep(1.5 - (time() - stime))
+rtime = 3.5 - (time() - stime)
+print("Waiting", rtime, "seconds...")
+sleep(rtime)
 print("Done!")
 server.terminate()
 server.join()
