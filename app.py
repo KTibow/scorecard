73936c543138d6bf599804b8b2659fa6d889c4f9
Kendell R <KTibow@users.noreply.github.com>
2020-10-26 11:48:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 import mimetypes
 import os
 from random import randint
-from time import time
+import time
 from flask import (
     Flask,
     g,
@@ -25,7 +25,6 @@ from urllib.parse import quote
 import requests
 
 # Service worker
-from time import sleep as tm_sleep
 import threading
 
 # Init flask
@@ -42,7 +41,7 @@ else:
 
 def sleep(timefor):
     for _i in range(round(timefor * 16.0)):
-        tm_sleep(1 / 16)
+        time.sleep(1 / 16)
 
 
 comm_num = 0
@@ -158,10 +157,10 @@ def track_view(page, ip, agent):
 @app.before_request
 def before_req():
     if "debuggy" not in globals():
-        g.before_before_request_time = time() * 1000
-        g.middle_before_request_time = time() * 1000
-        g.after_before_request_time = time() * 1000
-        g.after_after_request_time = time() * 1000
+        g.before_before_request_time = time.time() * 1000
+        g.middle_before_request_time = time.time() * 1000
+        g.after_before_request_time = time.time() * 1000
+        g.after_after_request_time = time.time() * 1000
         if request.headers["X-Forwarded-Proto"] == "http":
             return redirect(request.url.replace("http", "https"), code=301)
         ua = None
@@ -172,9 +171,9 @@ def before_req():
         ip = request.headers["X-Forwarded-For"]
         print("Hit from " + ip + ua_add)
         chunks = request.url.split("/")
-        g.middle_before_request_time = time() * 1000
+        g.middle_before_request_time = time.time() * 1000
         track_view("/".join(chunks[2 : len(chunks)]), ip, ua)
-        g.after_before_request_time = time() * 1000
+        g.after_before_request_time = time.time() * 1000
 
 
 @app.after_request
@@ -190,21 +189,22 @@ def after_req(response):
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
     if "debuggy" not in globals():
-        g.after_after_request_time = time() * 1000
-        response.headers[
-            "Server-Timing"
-        ] = 'beforereq;desc="Process redirect and log";dur='
-        response.headers["Server-Timing"] += str(
+        g.after_after_request_time = time.time() * 1000
+        server_timing = 'beforereq;desc="Process redirect and log";dur='
+        server_timing += str(
             round(g.middle_before_request_time - g.before_before_request_time, 1)
         )
-        response.headers["Server-Timing"] += ', track;desc="Track pageview";dur='
-        response.headers["Server-Timing"] += str(
+        server_timing += ', track;desc="Track pageview";dur='
+        server_timing += str(
             round(g.after_before_request_time - g.middle_before_request_time, 1)
         )
-        response.headers["Server-Timing"] += ', process;desc="Render stuff";dur='
-        response.headers["Server-Timing"] += str(
+        server_timing += ', process;desc="Render stuff";dur='
+        server_timing += str(
             round(g.after_after_request_time - g.after_before_request_time, 1)
         )
+        response.headers[
+            "Server-Timing"
+        ] = server_timing
     return response
 
 
