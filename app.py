75fd039501b3d2caaca5ccc4dcc192cc0a64a004
Kendell R <KTibow@users.noreply.github.com>
2020-10-26 17:34:39 -0700

    Lots of flake8 fixes

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-"""
-This is the scorecard Heroku app. Look at the README for more details.
-"""
+"""This is the scorecard Heroku app. Look at the README for more details."""
 
 # ============== INIT ==============
 
@@ -37,11 +35,11 @@ app = Flask(__name__, template_folder="game")
 minify(app=app, html=True, js=True, cssless=True, static=True, caching_limit=0)
 # Init github
 if os.getenv("GITHUB_VERSION_PAT") is not None:
-    gg = Github(os.getenv("GITHUB_VERSION_PAT"))
-    rep = gg.get_repo("KTibow/scorecard")
+    github_instance = Github(os.getenv("GITHUB_VERSION_PAT"))
+    rep = github_instance.get_repo("KTibow/scorecard")
 else:
-    gg = Github()
-    rep = gg.get_repo("KTibow/scorecard")
+    github_instance = Github()
+    rep = github_instance.get_repo("KTibow/scorecard")
 
 
 def sleep(timefor):
@@ -56,12 +54,12 @@ def find_commit():
     # Declarations
     print("Starting commit finder")
     global comm_num
-    global gg
+    global github_instance
     prevcomm = -1
     while True:
         # Get rate limiting and log if it's divisible by 10
-        rate_limit = gg.rate_limiting[1]
-        rate_limit_remaining = gg.rate_limiting[0]
+        rate_limit = github_instance.rate_limiting[1]
+        rate_limit_remaining = github_instance.rate_limiting[0]
         rate_limit_used = rate_limit - rate_limit_remaining
         if (rate_limit_used % 10) == 0:
             print(
@@ -159,7 +157,7 @@ def track_view(page, ip, agent):
 
 @app.before_request
 def before_req():
-    if "debuggy" not in globals():
+    if "debugithub_instancey" not in globals():
         flask_global.before_before_request_time = time.time() * 1000
         flask_global.middle_before_request_time = time.time() * 1000
         flask_global.after_before_request_time = time.time() * 1000
@@ -191,7 +189,7 @@ def after_req(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
-    if "debuggy" not in globals():
+    if "debugithub_instancey" not in globals():
         flask_global.after_after_request_time = time.time() * 1000
         server_timing = 'beforereq;desc="Process redirect and log";dur='
         server_timing += str(
@@ -268,9 +266,9 @@ def genid(username):
             group_database = json.load(open("groups.db", "r"))
         except FileNotFoundError:
             group_database = []
-        for gy in group_database:
-            group_database[group_database.index(gy)] = [
-                x if x != oid else nid for x in gy
+        for group in group_database:
+            group_database[group_database.index(group)] = [
+                x if x != oid else nid for x in group
             ]
         print(group_database)
         json.dump(group_database, open("groups.db", "w"))
@@ -312,42 +310,40 @@ def addid(exist, new):
         group_database = json.load(open("groups.db", "r"))
     except FileNotFoundError:
         group_database = []
-    for gy in group_database:
-        if exist in gy and new in gy:
+    for group in group_database:
+        if exist in group and new in group:
             return "already"
-    comp = [i for x in group_database for i in x]
+    comp = [user_id for group in group_database for user_id in group]
     if exist in comp and new in comp:
         newgp = []
-        for gy in group_database:
-            if new in gy:
-                newgp = gy
-        for gy in group_database:
-            if exist in gy:
-                group_database[group_database.index(gy)] = (
-                    gy + newgp[1 : len(newgp)]
+        for group in group_database:
+            if new in group:
+                newgp = group
+        for group in group_database:
+            if exist in group:
+                group_database[group_database.index(group)] = (
+                    group + newgp[1 : len(newgp)]
                 )
                 group_database.remove(newgp)
                 print(group_database)
                 json.dump(group_database, open("groups.db", "w"))
                 return "merge"
     elif exist in comp and new not in comp:
-        for gy in group_database:
-            if exist in gy:
-                group_database[group_database.index(gy)].append(new)
+        for group in group_database:
+            if exist in group:
+                group_database[group_database.index(group)].append(new)
                 print(group_database)
                 json.dump(group_database, open("groups.db", "w"))
                 return "addnew"
     elif exist not in comp and new in comp:
-        for gy in group_database:
-            if new in gy:
-                group_database[group_database.index(gy)].append(exist)
+        for group in group_database:
+            if new in group:
+                group_database[group_database.index(group)].append(exist)
                 print(group_database)
                 json.dump(group_database, open("groups.db", "w"))
                 return "addexist"
     else:
-        # Best: Give you answer, finish game
-        # Most: Give you what number isn't
-        # Couple: Nothing
+        # The best make you win, most tell you what it's not, and some don't give you anything.
         infodict = {"rightnum": str(randint(1, 1000))}
         fyet = False
         for tletter in "ABCD":
@@ -371,30 +367,51 @@ def addid(exist, new):
 
 @app.route("/gids/<uid>")
 def fids(uid):
+    """
+    Find all people in a group.
+
+    Args:
+        uid: The user ID.
+
+    Returns:
+        A HTML string for which people are in the group.
+    """
     try:
         group_database = json.load(open("groups.db", "r"))
     except FileNotFoundError:
         group_database = []
-    comp = [i for x in group_database for i in x]
+    comp = [user_id for group in group_database for user_id in group]
     if uid not in comp:
         return "You currently don't have anyone in your group."
-    for gy in group_database:
-        if uid in gy:
+    for group in group_database:
+        if uid in group:
             try:
                 id_database = json.load(open("ids.db", "r"))
             except FileNotFoundError:
                 id_database = {}
-            gy = gy[1 : len(gy)]
-            inv_map = {value: key for key, value in id_database.items()}
-            mgy = [inv_map[g] for g in gy.copy()]
+            group = group[1 : len(group)]
+            inv_map = {user_id: name for name, user_id in id_database.items()}
+            mgroup = [inv_map[person] for person in group.copy()]
             return (
                 "In your group, there's these people:"
-                + f'<span style="color: deepskyblue;">{", ".join(mgy)}</span>'
+                + f'<span style="color: deepskyblue;">{", ".join(mgroup)}</span>'
             )
 
 
 @app.route("/cardstatus/<uid>/<cardnum>")
 def checkcard(uid, cardnum):
+    """
+    Check a card number for a user ID.
+
+    Args:
+        uid: The user ID.
+        cardnum: The card ID.
+
+    Returns:
+        0 if the card is right.
+        1 if it should give the player a hint.
+        2 if it's not a helpful card.
+    """
     try:
         group_database = json.load(open("groups.db", "r"))
     except FileNotFoundError:
@@ -409,6 +426,15 @@ def checkcard(uid, cardnum):
 
 @app.route("/rightnum/<uid>")
 def rightnum(uid):
+    """
+    Find the right number based on a user ID.
+
+    Args:
+        uid: The user ID.
+
+    Returns:
+        The correct number for that ID.
+    """
     try:
         group_database = json.load(open("groups.db", "r"))
     except FileNotFoundError:
@@ -416,19 +442,18 @@ def rightnum(uid):
     comp = [user_id for group in group_database for user_id in group]
     if uid not in comp:
         return "-1"
-    for gy in group_database:
-        if uid in gy:
-            return gy[0]["rightnum"]
+    for group in group_database:
+        if uid in group:
+            return group[0]["rightnum"]
 
 
 # ========== BROWSER FILES ==========
-for browser_file in walk():
-    print(browser_file)
-    if browser_file[1] != "/sw.js":
+for folder_name, file_name in walk():
+    if file_name != "/sw.js":
         app.add_url_rule(
-            browser_file[1],
-            browser_file[1],
-            make_sender(browser_file[1], browser_file[0]),
+            file_name,
+            file_name,
+            make_sender(file_name, folder_name),
         )
 
 
@@ -454,9 +479,9 @@ def make_service_worker():
             link = f"{link}, "
         swlist += link
     global comm_num
-    sw = render_template(
+    service_worker = render_template(
         "browserfiles/sw.js", urls=swlist, version=str(comm_num)
     )
-    respo = app.make_response(sw)
-    respo.mimetype = "application/javascript"
-    return respo
+    response = app.make_response(service_worker)
+    response.mimetype = "application/javascript"
+    return response
