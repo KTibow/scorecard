76a398d2ab1b39918c7605ab4088eac815ab6b44
Kendell R <KTibow@users.noreply.github.com>
2020-11-11 14:19:21 -0800

    Fix URL

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['Running on http://127.0.0.1:5000'],
+      url: ['http://127.0.0.1:5000'],
       startServerCommand: 'python3 lighthouse_boot.py',
       startServerReadyPattern: 'Running on http://127.0.0.1:5000',
       startServerReadyTimeout: 5000
