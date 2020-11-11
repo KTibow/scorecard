732536c2fb066b50f2a706e018a95c18845c2a69
Kendell R <KTibow@users.noreply.github.com>
2020-11-11 14:17:03 -0800

    Add more Lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,10 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:5000/'],
+      url: ['Running on http://127.0.0.1:5000'],
       startServerCommand: 'python3 lighthouse_boot.py',
+      startServerReadyPattern: 'Running on http://127.0.0.1:5000',
+      startServerReadyTimeout: 5000
     },
     upload: {
       target: 'temporary-public-storage',
