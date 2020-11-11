41d8b1d92ed33c77bac346e4ee08b99d984f5fa0
Kendell R <KTibow@users.noreply.github.com>
2020-11-11 10:48:44 -0800

    Little fixes and run prettier

--- game/browserfiles/sw.js
+++ game/browserfiles/sw.js
@@ -1,13 +1,14 @@
-var cacheName = 'clue-card-v{{version}}';
+var cacheName = "clue-card-v{{version}}";
 function log_message(message, color, object) {
-  console.log(
-    "%c sw.js: %c" + message,
-    "color: orange;",
-    "color: " + color,
-    object || "");
+    console.log(
+        "%c sw.js: %c" + message,
+        "color: orange;",
+        "color: " + color,
+        object || ""
+    );
 }
 log_message("👋 Hello there!", "green");
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
     if (navigator.onLine) {
         log_message("⏭️ Online, not waiting.", "darkslateblue");
         self.skipWaiting();
@@ -15,44 +16,56 @@ self.addEventListener('install', (e) => {
     log_message("🔻 Installing ...", "yellow");
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
-            log_message("⬇ Caching caches...", "coral", [{{urls}}]);
-            return cache.addAll([{{urls}}]);
+            var cache_urls = JSON.parse("{{urls}}");
+            log_message("⬇ Caching caches...", "coral", cache_urls);
+            return cache.addAll(cache_urls);
         })
     );
     e.waitUntil(
         caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== cacheName) {
-                    log_message("👋 See you later " + key, "coral");
-                    return caches.delete(key);
-                }
-            }));
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== cacheName) {
+                        log_message("👋 See you later " + key, "coral");
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
     );
     log_message("✅ Done installing!", "green");
 });
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
     log_message("🌎 We got a (no, not 🐟) fetch!", "slateblue", event.request);
-    if (!event.request.url.includes("makeid") &&
+    if (
+        !event.request.url.includes("makeid") &&
         !event.request.url.includes("addid") &&
         !event.request.url.includes("gids") &&
-        !event.request.url.includes("cardstatus")) {
-        caches.open(cacheName).then(function(cache) {
-            log_message("🔻 Caching for later use:", "green", event.request.url);
-            cache.add(event.request.url);
-        }).catch(function() {
-            log_message("❌ Error caching", "red", event.request.url);
-        });
+        !event.request.url.includes("cardstatus")
+    ) {
+        caches
+            .open(cacheName)
+            .then(function (cache) {
+                log_message(
+                    "🔻 Caching for later use:",
+                    "green",
+                    event.request.url
+                );
+                cache.add(event.request.url);
+            })
+            .catch(function () {
+                log_message("❌ Error caching", "red", event.request.url);
+            });
     }
     event.respondWith(
-        fetch(event.request).catch(function() {
+        fetch(event.request).catch(function () {
             log_message("↩ Returning cache for", "blue", event.request.url);
-            return caches.match(event.request.url).then(function(resty) {
+            return caches.match(event.request.url).then(function (resty) {
                 if (!resty) {
                     log_message("❌ Page not found:", "red", event.request.url);
-                    return caches.match("/404")
+                    return caches.match("/404");
                 }
-            })
+            });
         })
     );
 });
