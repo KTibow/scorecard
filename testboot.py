08967c9d08dc28a79a15d647ba85e26e1a4e74ff
Kendell R <KTibow@users.noreply.github.com>
2020-11-11 14:31:34 -0800

    Minor testboot.py improvements

--- testboot.py
+++ testboot.py
@@ -1,16 +1,17 @@
 print("Loading...")
 import app
+
 app.debug_mode_enabled = True
 from multiprocessing import Process
 from time import sleep
 from urllib.request import urlopen
 
-server = Process(target=app.run)
+server = Process(target=app.app.run)
 server.start()
 print("Waiting for 2 seconds...")
 sleep(2)
 print("Pinging the server...")
-rpy = urlopen("http://127.0.0.1:5000/", timeout=1)
+urlopen("http://127.0.0.1:5000/", timeout=1)
 print("Done!")
 server.terminate()
 server.join()
