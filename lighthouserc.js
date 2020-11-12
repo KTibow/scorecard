46cdf0fa285d9cce14179e1ec758e45a32f3b713
Kendell R <KTibow@users.noreply.github.com>
2020-11-12 07:18:37 -0800

    Add pupeteer

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,7 @@ module.exports = {
             startServerCommand: "python3 lighthouse_boot.py",
             startServerReadyPattern: "Running on http://127.0.0.1:5000",
             startServerReadyTimeout: 5000,
+            puppeteerScript: "login-script.js"
         },
         upload: {
             target: "temporary-public-storage",
