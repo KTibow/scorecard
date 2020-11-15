4848ccd4e9c0054237f26e41650c003937afe245
Kendell R <KTibow@users.noreply.github.com>
2020-11-15 10:02:02 -0800

    :truck: Clean up root dir

--- assets/lighthouserc.js
+++ assets/lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
                 "http://127.0.0.1:5000",
                 "http://127.0.0.1:5000/cluecard/12345",
             ],
-            startServerCommand: "python3 lighthouse_boot.py",
+            startServerCommand: "python3 assets/lighthouse_boot.py",
             startServerReadyPattern: "Running on http://127.0.0.1:5000",
             startServerReadyTimeout: 5000,
         },
