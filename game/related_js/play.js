e12fb81753ad6e4d327f2c0d2ff0c9fd4b207981
Kendell R <KTibow@users.noreply.github.com>
2020-11-14 17:34:12 -0800

    :sparkles: :art: :fire: Improve card check code and remove final number and migrate to fetch

--- game/related_js/play.js
+++ game/related_js/play.js
@@ -71,129 +71,73 @@ function getGroup() {
     }
 }
 setInterval(getGroup, 400);
-var xmlhttpy = new XMLHttpRequest();
-var urly = "/rightnum/" + userId;
-var nopes = [];
-for (var i = 1; i <= 1000; i++) {
-    nopes.push(i);
-}
-xmlhttpy.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        nopes.pop(Number(this.responseText));
-    }
-};
-xmlhttpy.open("GET", urly, true);
-xmlhttpy.send();
-function choose(choices) {
-    var index = Math.floor(Math.random() * choices.length);
-    return choices[index];
-}
 function getCard() {
-    document.getElementById("attmpts").innerHTML = String(
-        Number(document.getElementById("attmpts").innerHTML) + 1
-    );
+    var attemptsElement = document.getElementById("attmpts");
+    var attemptsSoFar = Number(attemptsElement.innerHTML) + 1;
+    attemptsElement.innerHTML = String(attemptsSoFar);
     var renderPopupTimeout = 0;
-    if (Number(document.getElementById("attmpts").innerHTML) == 16) {
+    if (attemptsSoFar == 16) {
         openOverlay(
             "It looks like you've done all of the cards. Make sure you aren't repeating any."
         );
         renderPopupTimeout = 2000;
     }
-    function renderPopup() {
-        var xmlhttp = new XMLHttpRequest();
-        var url =
-            "/cardstatus/" +
-            userId +
-            "/" +
-            document.getElementById("cardname").value;
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText == "0") {
-                    var xmlhttp2 = new XMLHttpRequest();
-                    var url2 = "/rightnum/" + userId;
-                    xmlhttp2.onreadystatechange = function () {
-                        if (this.readyState == 4 && this.status == 200) {
-                            openOverlay(
-                                "That's the right clue! The number is " +
-                                    this.responseText +
-                                    ". Share it with your friends, and they can verify it!"
+    setTimeout(() => {
+        fetch(
+            `/cardstatus/${userId}/${document.getElementById("cardname").value}`
+        )
+            .then((result) => {
+                return result.text();
+            })
+            .then((outcome) => {
+                switch (outcome) {
+                    case "correct":
+                        openOverlay(
+                            "That's the right clue! Your group can see you're finished now."
+                        );
+                        var confettiConfig = {
+                            particleCount: 100,
+                            startVelocity: 30,
+                            spread: 360,
+                        };
+                        for (confettiDelay of [10, 510, 1010]) {
+                            setTimeout(
+                                confetti,
+                                confettiDelay,
+                                Object.assign(
+                                    {
+                                        origin: {
+                                            x: Math.random(),
+                                            y: Math.random() - 0.2,
+                                        },
+                                    },
+                                    confettiConfig
+                                )
                             );
-                            setTimeout(confetti, 10, {
-                                particleCount: 100,
-                                startVelocity: 30,
-                                spread: 360,
-                                origin: {
-                                    x: Math.random(),
-                                    y: Math.random() - 0.2,
-                                },
-                            });
-                            setTimeout(confetti, 510, {
-                                particleCount: 100,
-                                startVelocity: 30,
-                                spread: 360,
-                                origin: {
-                                    x: Math.random(),
-                                    y: Math.random() - 0.2,
-                                },
-                            });
-                            setTimeout(confetti, 1010, {
-                                particleCount: 100,
-                                startVelocity: 30,
-                                spread: 360,
-                                origin: {
-                                    x: Math.random(),
-                                    y: Math.random() - 0.2,
-                                },
-                            });
-                            var xmlhttp3 = new XMLHttpRequest();
-                            var url3 = "/finished/" + userId;
-                            xmlhttp3.open("GET", url3, true);
-                            xmlhttp3.send();
                         }
-                    };
-                    xmlhttp2.open("GET", url2, true);
-                    xmlhttp2.send();
-                } else if (this.responseText == "1") {
-                    if (nopes.length < 1) {
+                        fetch(`/finished/${userId}`);
+                        break;
+                    case "regular":
+                        fetch(
+                            `/nopecard/${
+                                document.getElementById("cardname").value
+                            }/${userId}`
+                        )
+                            .then((result) => {
+                                return result.text();
+                            })
+                            .then((cardNum) => {
+                                openOverlay(
+                                    `This is a normal card. Don't go looking for card ${cardNum}`
+                                );
+                            });
+                        break;
+                    case "invalid":
                         openOverlay(
-                            "Ooops! You missed out on the right one from 1-1000."
+                            "That's an invalid clue. Make sure you're in a group."
                         );
-                    } else {
-                        var choice = choose(nopes);
-                        openOverlay(
-                            "This is a normal clue, and it tells you that it's not " +
-                                String(choice)
-                        );
-                        nopes.pop(choice);
-                    }
-                } else if (this.responseText == "2") {
-                    openOverlay("This clue doesn't help you.");
-                } else if (this.responseText == "invalid") {
-                    openOverlay(
-                        "That's an invalid clue. Make sure you're in a group."
-                    );
+                        break;
                 }
-            }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-    }
-    setTimeout(renderPopup, renderPopupTimeout);
-}
-function checkNumber() {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "/rightnum/" + userId;
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (
-                this.responseText == document.getElementById("numbername").value
-            ) {
-                openOverlay("Yup, this person actually got it!");
-            } else {
-                openOverlay("That's an invalid number. Double-check it.");
-            }
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+            });
+    }, renderPopupTimeout);
 }
