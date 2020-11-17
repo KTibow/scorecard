32043c150e1d42c5a49cca0b9b68da713451dcdb
Kendell R <KTibow@users.noreply.github.com>
2020-11-16 18:34:39 -0800

    ✨🚧 Try to finish up localStorage support

--- game/related_js/welcome.js
+++ game/related_js/welcome.js
@@ -13,8 +13,34 @@ function registerUser() {
             window.location = result;
         });
 }
-var sheet = document.createStyleSheet();
-if (localStorage.getItem("card") != null) {
-    sheet.addRule("#content", "display: none;");
-    sheet.addRule("body", "background: var(--primary-color);");
+if (localStorage.getItem("username") != null) {
+    var sheet = document.createElement("style");
+    sheet.innerHTML = `
+#content {
+    display: none;
+}
+#loading {
+    display: block;
+    background: var(--primary-color);
+    width: 50vw;
+    height: 50vh;
+    margin: 25vh 25vw;
+    animation: infinite 1s jiggle;
+}
+p {
+    font-size: 2em;
+    transform: translateY(calc(50% - 1em));
+    height: 100%;
+    color: white;
+    text-align: center;
+}
+html, body {
+    margin: 0;
+    height: 100%;
+}`;
+    document.body.appendChild(sheet);
+    document.getElementById("username").value = localStorage.getItem(
+        "username"
+    );
+    registerUser();
 }
