d9b80ea79799d1f2ff7af1fe1832ddc8a149fdc7
Kendell R <KTibow@users.noreply.github.com>
2020-11-17 19:01:18 -0800

    :green_heart: CI and improvements

--- assets/lighthouserc.js
+++ assets/lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
         collect: {
             url: [
                 "http://127.0.0.1:5000",
-                "http://127.0.0.1:5000/cluecard/12345",
+                "http://127.0.0.1:5000/cluecard/test",
             ],
             startServerCommand: "python3 -m assets.lighthouse_boot",
             startServerReadyPattern: "Running on http://127.0.0.1:5000",
