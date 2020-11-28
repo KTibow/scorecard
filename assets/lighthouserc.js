ce7c6d988937b206a173fe93cab3373f0b9459e3
Kendell R <KTibow@users.noreply.github.com>
2020-11-27 18:55:28 -0800

    🔧 Don't use lighthouse_boot

--- assets/lighthouserc.js
+++ assets/lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
                 "http://127.0.0.1:5000",
                 "http://127.0.0.1:5000/cluecard/test",
             ],
-            startServerCommand: "python3 -m assets.lighthouse_boot",
+            startServerCommand: "python3 app.py",
             startServerReadyPattern: "Running on http://127.0.0.1:5000",
             startServerReadyTimeout: 10000,
         },
